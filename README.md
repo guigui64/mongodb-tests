cffaec4e02fd0eec63404259e81e84a83db26557
guigui64 <guillaume.comte10@gmail.com>
2019-03-11 23:42:00 +0100

    Credentials + async/await + insert/find

---
+++ README.md
@@ -0,0 +1,10 @@
+# MongoDB tests
+
+## Credentials
+MongoDB user credentials must be put into an executable (use `chmod +x`) shell file. For example:
+
+```console
+#! /bin/bash
+export MONGO_USER='toto'
+export MONGO_PASSWORD='secret'
+```
--- server.js
+++ server.js
@@ -1,20 +1,32 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const MONGO_USER = 'guillaume';
-const MONGO_PASSWORD = 'ZHAu9q51xahndAIG';
+const MONGO_USER = encodeURIComponent(process.env.MONGO_USER);
+const MONGO_PASSWORD = encodeURIComponent(process.env.MONGO_PASSWORD);
 
 const uri = `mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@gcomte-mdb-cluster-ehume.mongodb.net/test?retryWrites=true`;
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const db = client.db("sandbox-db");
-  const collection = db.collection("sandbox-users");
-  const cursor = collection.find({});
-  function iterateFunc(doc) {
-    console.log('document', JSON.stringify(doc, null, 4));
- }
- function errorFunc(error) {
-    console.log('error', error);
- }
- cursor.forEach(iterateFunc, errorFunc);
-  client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true
 });
+
+(async function () {
+  try {
+
+    await client.connect();
+    const db = client.db("sandbox-db");
+    const collection = db.collection("users");
+
+    collection.insertOne({email: 'guillaume.comte10@gmail.com', example: true}).catch(err => console.error('insert error', err.name, err.errmsg, err.code));
+
+    const docs = await collection.find({
+      email: {
+        $regex: /^guillaume\.comte10/
+      }
+    }).toArray();
+    console.log('found docs', docs);
+
+    client.close();
+
+  } catch (err) {
+    console.error(error);
+  }
+})();
